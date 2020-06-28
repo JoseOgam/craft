6daa0292c6955949bf889dbeb2194a767457814f
JoseOgam <jotienoochieng@gmail.com>
2020-06-28 15:19:01 -0400

    initial commits

--- server.js
+++ server.js
@@ -7,27 +7,24 @@ var port = process.env.PORT || 3000;
 app.use(express.json());
 
 //create user using post method
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   var user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 //Get user using get method
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
+app.get("/users", async (req, res) => {
+  try {
+    var users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 app.listen(port, () => {
   console.log(`server is up on port ${port} `);
